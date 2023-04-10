9ec91a4957ee4932bbda7f7f0b2fc3268aab1525
Slavik04 <slavakhotyanovich9@gmail.com>
2023-04-10 03:07:06 +0300

    Create a project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container wrapper">
+          <Header/>
+        <nav className="nav">
+          <div>
+            <a href="#">Profile</a>
+          </div>
+          <div>
+            <a href="#">Messages</a>
+          </div>
+          <div>
+            <a href="#">News</a>
+          </div>
+          <div>
+            <a href="#">Music</a>
+          </div>
+          <div>
+            <a href="#">Settings</a>
+          </div>
+        </nav>
+        <div className="content">
+          Main content
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+      <header className="header">
+        <img src="https://cdn.freelogodesign.org/files/fc246148d2544451a6c7d150b9467cee/thumb/logo_200x200.png?v=638166814420000000" alt="logo"/>
+      </header>
+  );
+};
+
+export default Header;
